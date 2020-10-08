4cbb3181a61629f799fc39284d8a9f9528c7ab1d
jek55rus jek55rus <markin95evgeny@gmail.com>
2020-10-08 15:12:09 +0300

    added write

--- reader.py
+++ reader.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 class Reader(object):
-	"""docstring for ClassName"""
 	def __init__(self):
 		pass
 
@@ -30,10 +29,9 @@ class Reader(object):
 
 	def is_valid(self, df):
 		'''
-		Checks if input dataframe's columns are in the right format 
+		Checks if input dataframe's columns are in the right format
+		mode -- read or write 
 		'''
-		# print([col_name[1:] for col_name in df.columns[1:]])
-		# input('stop')
 		coords_nums = [int(col_name[1:]) for col_name in df.columns[1:]]
 		is_asc = coords_nums == sorted(coords_nums)
 		if is_asc == False:
@@ -48,5 +46,17 @@ class Reader(object):
 				print('Wrong column name format')
 				return False
 		return True
+
+	def write(self, out_df, file_path):
+		'''writes df to file, returns nothing or None'''
+		file_extension = file_path.split('.')[1]
+		if file_extension == 'csv':
+			out_df = pd.to_csv(file_path) 
+		elif file_extension == 'xlsx' or file_extension == 'xls':
+			out_df = pd.to_excel(file_path) 
+		else:
+			print('Unsupported format')
+			return
+
 
 
